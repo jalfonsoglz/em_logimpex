83508eb743799a28141236795a9c3bbee785d709
DesarrolloPtree <alfonsoglzslz@gmail.com>
2022-04-22 12:38:29 -0500

    15.0.0.0.4
    No create many2one fields

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
     'author': "Alfonso Gonzalez (alfonso@ptree.com.mx)",
     'website': "https://ptree.com.mx/",
     'category': 'Customizations',
-    'version': '15.0.0.0.3',
+    'version': '15.0.0.0.4',
     'license': "AGPL-3",
     'sequence': "-100",
     'depends': [
